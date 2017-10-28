f45aa34504112d84c571a752e5915f4101ac214e
ThijsLaptop <thijs.wijnen@gmail.com>
2017-10-28 14:35:52 +0200

    added gewicht to user

--- Server/Server.cs
+++ Server/Server.cs
@@ -51,9 +51,9 @@ namespace Server {
                 {
                     DateTime time = new DateTime(0);
                     string username = Encoding.Default.GetString(new SHA256Managed().ComputeHash(Encoding.Default.GetBytes("admin")));
-                    users.Add(new User(username, username, "Root",  time.ToString(), true, UserType.Doctor));
+                    users.Add(new User(username, username, "Root",  time.ToString(), true, 100, UserType.Doctor));
                     username = Encoding.Default.GetString(new SHA256Managed().ComputeHash(Encoding.Default.GetBytes("test")));
-                    users.Add(new User(username, username, "Patient",  time.ToString(), true, UserType.Client));
+                    users.Add(new User(username, username, "Patient",  time.ToString(), true, 100, UserType.Client));
                     File.WriteAllText(usersPath, JsonConvert.SerializeObject(users));
                 }
             }
